66fc7f84635734fd02c4a96c7700e58f9be54440
Caique Coelho Pinheiro <caiquepinheiro69@gmail.com>
2024-11-12 20:14:34 -0300

    Create a Product and Supplier Class and Interface

---
+++ src/PlayTechShop.Domain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using PlayTechShop.Domain.Entities.Base;
+
+namespace PlayTechShop.Domain.Entities;
+
+public class Product : EntityBase
+{
+    public string Description { get; set; }=string.Empty;
+    public DateTime ValidadeDoProduto { get; set; }
+
+    //Preco
+    public decimal Price { get; set; }
+
+    // Quantidade disponível em estoque
+    public int Quantity { get; set; }
+
+    // Código único do produto
+    public string ProductCode { get; set; } = string.Empty;
+
+    //Expiracao do produto
+    public DateTime ExpirationDate { get; set; }
+
+    //Relacionamentocom a classe Suppliers
+    public Suppliers SupplierId { get; set; }
+
+
+}
---
+++ src/PlayTechShop.Domain/Entities/Suppliers.cs
@@ -0,0 +1,25 @@
+﻿using PlayTechShop.Domain.Entities.Base;
+
+namespace PlayTechShop.Domain.Entities;
+
+public class Suppliers : EntityBase
+{
+    public string Nome { get; set; }
+
+    // Endereço do fornecedor
+    public string Address { get; set; } = string.Empty;
+
+    // Telefone de contato do fornecedor
+    public string ContactNumber { get; set; } = string.Empty;
+
+    // E-mail de contato do fornecedor
+    public string Email { get; set; } = string.Empty;
+
+    // Pessoa de contato principal do fornecedor
+    public string ContactPerson { get; set; } = string.Empty;
+
+
+    // Relacionamente com a classe Product
+    public virtual ICollection<Product> Products { get; set; }
+}
+
